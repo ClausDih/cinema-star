214f64d92189ece81a2d1d19e242cdef2f8f6141
ClausDih <claudiana.santos@sptech.school>
2026-09-06 15:32:25 -0300

    Adiciona atualização de filme

--- backend/src/main/java/school/sptech/cinema_star/CinemaStarController.java
+++ backend/src/main/java/school/sptech/cinema_star/CinemaStarController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 import java.sql.PreparedStatement;
@@ -78,6 +79,42 @@ public class CinemaStarController {
         return ResponseEntity.status(201).body(novoFilme);
     }
 
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CinemaStar> atualizarFilme(@PathVariable Integer id, @RequestBody CinemaStar filmeAtualizado) {
+
+        String sql = "SELECT * FROM cinema WHERE id = ?";
+        List<CinemaStar> filmes = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(CinemaStar.class), id);
+
+        if (filmes.isEmpty()) {
+            return ResponseEntity.status(404).build();
+        }
+
+        if (filmeAtualizado.getTitulo() == null || filmeAtualizado.getTitulo().isBlank() ||
+                filmeAtualizado.getDiretor() == null || filmeAtualizado.getDiretor().isBlank() ||
+                filmeAtualizado.getGenero() == null || filmeAtualizado.getGenero().isBlank() ||
+                filmeAtualizado.getAnoLancamento() == null || filmeAtualizado.getAnoLancamento() < 1888 ||
+                filmeAtualizado.getNota() == null || filmeAtualizado.getNota() < 0 || filmeAtualizado.getNota() > 10){
+
+            return ResponseEntity.status(400).build();
+        }
+
+    String sqlAtualizar = "UPDATE cinema SET titulo = ?, diretor = ?, ano_lancamento = ?, genero = ?, nota = ? WHERE id = ?";
+        jdbcTemplate.update(sqlAtualizar,
+                filmeAtualizado.getTitulo(),
+                filmeAtualizado.getDiretor(),
+                filmeAtualizado.getAnoLancamento(),
+                filmeAtualizado.getGenero(),
+                filmeAtualizado.getNota(),
+                id
+                );
+
+    String sqlBuscar = "SELECT * FROM cinema WHERE id = ?";
+    CinemaStar filme = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(CinemaStar.class), id);
+
+    return ResponseEntity.status(200).body(filme);
+
+    }
 
 
 }
